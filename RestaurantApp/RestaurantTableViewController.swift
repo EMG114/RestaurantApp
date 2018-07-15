435a80a3755d476b162fb71579647e8267e7f3ac
Erica Gutierrez <egutierrez411@gmail.com>
2018-07-14 21:45:48 -0400

    adding cell selection interaction

--- RestaurantApp/RestaurantTableViewController.swift
+++ RestaurantApp/RestaurantTableViewController.swift
@@ -35,6 +35,30 @@ class RestaurantTableViewController: UITableViewController {
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         optionMenu.addAction(cancelAction)
         
+        let callActionHandler = {(action: UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+            
+        }
+
+       let callAction = UIAlertAction(title: "Call" + "123-000-\(indexPath.row)", style: .default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+       
+        let checkInAction = UIAlertAction(title: "Check in", style: .default, handler: {
+            (action:UIAlertAction!) -> Void in
+            
+            let cell = tableView.cellForRow(at: indexPath)
+                cell?.accessoryType = .checkmark })
+        
+        optionMenu.addAction(checkInAction)
+        
+        
+       
+        
+        
+        
+        
         //present and display menu
         present(optionMenu, animated: true, completion: nil)
     }
