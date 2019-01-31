f94457d82aa7c563b68e594f6902f3cdb789b536
Erica Gutierrez <egutierrez411@gmail.com>
2019-01-30 19:09:39 -0500

    cleaning some code

--- RestaurantApp/RestaurantTableViewController.swift
+++ RestaurantApp/RestaurantTableViewController.swift
@@ -12,88 +12,14 @@ class RestaurantTableViewController: UITableViewController {
     
     var restaurantNames = ["Boulud", "Momofuku Nishi", "Per Se"]
     var restaurantImages = ["restaurant.jpg", "restaurant2.jpg","restaurant3.jpg"]
+    var restaurantLocations = ["Hong Kong", "NY", "Hong Kong", "NY"]
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House"]
     var restaurantWasVisited = Array(repeating: false, count: 21)
-
-    
- 
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
         tableView.cellLayoutMarginsFollowReadableWidth = true
-     
-        
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
-      
-        if let popoverController = optionMenu.popoverPresentationController
-        {
-            if let cell = tableView.cellForRow(at: indexPath) {
-                popoverController.sourceView = cell
-                popoverController.sourceRect = cell.bounds
-            }
-        }
-        
-        //add action to menu
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        optionMenu.addAction(cancelAction)
-        
-        let callActionHandler = {(action: UIAlertAction!) -> Void in
-            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
-            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alertMessage, animated: true, completion: nil)
-            
-        }
-
-       let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .default, handler: callActionHandler)
-        optionMenu.addAction(callAction)
-       
-        let checkActionTitle = (restaurantWasVisited[indexPath.row]) ? "Undo Check in" : "Check in"
-        
-        let checkInAction = UIAlertAction(title: "Check in", style: .default, handler: {
-            (action:UIAlertAction!) -> Void in
-            
-            let cell = tableView.cellForRow(at: indexPath)
-                cell?.accessoryType = .checkmark
-            self.restaurantWasVisited[indexPath.row] = true
-            // Solution to exercise #1
-            // ---
-            // Toggle the accessoryType and the value of restaurantIsVisited[indexPath.row]
-            // If the value of self.restaurantIsVisited[indexPath.row] is true, we set the accessory type to .none.
-            
-            cell?.accessoryType = (self.restaurantWasVisited[indexPath.row]) ? .none : .checkmark
-            
-            // Solution to exercise #2
-            // ---
-            // Use the isHidden property to control the appearance of the heart icon
-            
-           // cell.heartImageView.isHidden = self.restaurantIsVisited[indexPath.row] ? false : true
-            
-          
-        })
-        
-        optionMenu.addAction(checkInAction)
-        
-        
-        //present and display menu
-        present(optionMenu, animated: true, completion: nil)
-        
-        tableView.deselectRow(at: indexPath, animated: false)
     }
 
     // MARK: - Table view data source
@@ -115,55 +41,89 @@ class RestaurantTableViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
 
-        // Configure the cell...
-        
         cell.nameLabel.text = restaurantNames[indexPath.row]
-       // cell.locationLabel
+        cell.locationLabel.text = restaurantLocations[indexPath.row]
+        cell.typeLabel.text = restaurantTypes[indexPath.row]
         cell.thumbnailImageView.image  = UIImage(named: restaurantImages[indexPath.row] )
         
-        // Solution to Exercise #1
-         cell.accessoryType = restaurantWasVisited[indexPath.row] ? .checkmark : .none
-        
-        // Solution to Exercise #2
-        // Use the isHidden property to control the appearance of the heart icon
-      //  cell.heartImageView.isHidden = restaurantWasVisited[indexPath.row] ? false : true
-        
-        if restaurantWasVisited[indexPath.row]{
-            cell.accessoryType = .checkmark } else {
-            cell.accessoryType = .none
-        }
-        
+        cell.accessoryType = restaurantWasVisited[indexPath.row] ? .checkmark : .none
 
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+        
+        if let popoverController = optionMenu.popoverPresentationController
+        {
+            if let cell = tableView.cellForRow(at: indexPath) {
+                popoverController.sourceView = cell
+                popoverController.sourceRect = cell.bounds
+            }
+        }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        let callActionHandler = {(action: UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+            
+        }
+        
+        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        let checkActionTitle = (restaurantWasVisited[indexPath.row]) ? "Undo Check in" : "Check in"
+        
+        let checkInAction = UIAlertAction(title: "Check in", style: .default, handler: {
+            (action:UIAlertAction!) -> Void in
+            
+            let cell = tableView.cellForRow(at: indexPath)
+            cell?.accessoryType = .checkmark
+            self.restaurantWasVisited[indexPath.row] = true
+        
+        })
+        
+        optionMenu.addAction(checkInAction)
+        
+        present(optionMenu, animated: true, completion: nil)
+        
+        tableView.deselectRow(at: indexPath, animated: false)
     }
-    */
+    
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") {
+            (action, sourceView, completionHandler) in
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            self.restaurantNames.remove(at: indexPath.row)
+            self.restaurantLocations.remove(at: indexPath.row)
+            self.restaurantTypes.remove(at: indexPath.row)
+            self.restaurantWasVisited.remove(at: indexPath.row)
+            self.restaurantImages.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            // Call completion handler to dismiss the action button
+            completionHandler(true)
+        }
+        let shareAction = UIContextualAction(style: .normal, title: "Share") { (action
+            , sourceView, completionHandler) in
+            let defaultText = "Just checking in at " + self.restaurantNames[indexPath.row]
+            let activityController: UIActivityViewController
+            if let imageToShare = UIImage(named: self.restaurantImages[indexPath.row])
+            {
+                activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
+            } else {
+                activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+            }
+            self.present(activityController, animated: true, completion: nil)
+            completionHandler(true)
+        }
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction, shareAction])
+        return swipeConfiguration
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
 
     /*
     // Override to support conditional rearranging of the table view.
